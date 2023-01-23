f34125bce8a39fdc005e807fdbb00a647d5cbc38
Jesse Gelardi <jesse.gelardi97@gmail.com>
2023-01-23 12:22:39 -0500

    class setup

---
+++ app.js
@@ -0,0 +1,19 @@
+class Card {
+    constructor(value, suit) {
+      this.value = number;
+      this.suit = suit;
+    }
+  }
+
+  class Deck {
+    constructor( ) {
+      this.deck = [ ];
+    }
+  }
+
+  class Player {
+    constructor(Player1, Player2) {
+      this.Player1 = Player1;
+      this.Player2 = Player2;
+    }
+  }
